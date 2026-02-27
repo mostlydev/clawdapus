63663fd458a9bf6bad92187bd1861e5fe4523556
Wojtek <wojtek@grabski.ca>
2026-02-27 18:23:15 -0500

    feat: add GoReleaser config and GitHub Actions release workflow

--- cmd/claw/root.go
+++ cmd/claw/root.go
@@ -1,9 +1,22 @@
 package main
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	version = "dev"
+	commit  = "none"
+)
 
 var rootCmd = &cobra.Command{
 	Use:          "claw",
 	Short:        "Infrastructure-layer governance for AI agent containers",
 	SilenceUsage: true,
+}
+
+func init() {
+	rootCmd.Version = fmt.Sprintf("%s (%s)", version, commit)
 }
